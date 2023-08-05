cc6c936a52a9d8800a1360bd4605ec2f2416902d
cijiugechu <cijiugechu@gmail.com>
2023-08-05 15:13:45 +0800

    chore: publish cjs build

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   external: ['@napi-rs/canvas', 'thumbhash-node', '@rollup/pluginutils'],
-  outExtension: () => ({
-    js: '.mjs',
-  }),
+  outExtension: ( { format } ) => {
+    return format === 'esm' ? { js: '.mjs' } : { js: '.cjs' }
+  },
 })
--- packages/core/tsup.debug.ts
+++ packages/core/tsup.debug.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
   clean: true,
   external: ['@napi-rs/canvas', 'thumbhash-node', '@rollup/pluginutils'],
-  outExtension: () => ({
-    js: '.mjs',
-  }),
+  outExtension: ( { format } ) => {
+    return format === 'esm' ? { js: '.mjs' } : { js: '.cjs' }
+  },
 })
